931d074279bd0dde3495aa08ebd435638683f252
tanzeelafaheem <tanzeelafaheem20@gmail.com>
2025-04-04 15:03:13 +0530

    updated CartPage

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,12 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
+import {useNavigate} from 'react-router-dom'
 
 export const ShopContext=createContext();
 const ShopContextProvider=(props)=>{
     const currency='$';
     const delivery_fee=10;
     const [cartItems,setCartItems]=useState({});
+    const navigate=useNavigate()
 
     const addToCart= async (itemId,size) => {
         if(!size){
@@ -41,10 +43,33 @@ const ShopContextProvider=(props)=>{
             }
         }return totalCount;
     }
+   const updateQuantity=async(itemId,size,quantity)=>{
+   let cartData=structuredClone(cartItems);
+   cartData[itemId][size]=quantity;
+   setCartItems(cartData);
+   }
+
+   const getCartAmount= () => {
+    let totalAmount=0;
+    for(const items in cartItems){
+        let itemInfo=products.find((product)=>product._id==items);
+        for(const item in cartItems[items]){
+            try {
+                if(cartItems[items][item]>0){
+                    totalAmount+=itemInfo.price* cartItems[items][item]
+                }
+            } catch (error) {
+                
+            }
+        }
+    }return totalAmount;
+   }
+
     const value={
     products,currency,delivery_fee,
     cartItems,addToCart,
-    getCartCount
+    getCartCount,updateQuantity,
+    getCartAmount,navigate
     }
     
     return(
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import { assets, products } from "../assets/frontend_assets/assets";
+import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
+  const { product, currency, cartItems,updateQuantity,navigate } = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
   return (
     <div>
-      
+      <div className="border-t pt-14 ">
+        <div className="text-2xl mb-3">
+          <Title text1={"YOUR"} text2={"CART"} />
+        </div>
+        <div>
+          {cartData.map((item, index) => {
+            const productData = products.find(
+              (product) => product._id === item._id
+            );
+            return (
+              <div
+                key={index}
+                className="py-4 border-t border-b text-gray-700 grid sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+              >
+                <div className="flex items-start gap-6">
+                  <img
+                    className="w-16 sm:w-20"
+                    src={productData.image[0]}
+                    alt=""
+                  />
+                  <div>
+                    <p className="text-sm sm:text-lg font-medium">
+                      {productData.name}
+                    </p>
+                    <div className="flex items-center gap-5 mt-2">
+                      <p>
+                        {currency}
+                        {productData.price}
+                      </p>
+                      <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                        {item.size}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="justify-self-end flex items-center gap-3">
+                  <input
+                   onChange={(e)=>e.target.value===''||e.target.value==='0'?null:updateQuantity(item._id,item.size,Number(e.target.value))}
+                    className="border rounded w-12 sm:w-16 px-2 py-1 text-center focus:outline-none focus:ring-2 focus:ring-orange-400"
+                    type="number"
+                    min={1}
+                    defaultValue={item.quantity}
+                  />
+                  <img
+                    onClick={()=>updateQuantity(item._id,item.size,0)}
+                    src={assets.bin_icon}
+                    alt="Delete"
+                    className="w-5 sm:w-6 cursor-pointer hover:scale-110 transition-transform duration-200"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex justify-end my-20">
+        <div className="w-full sm:w-[450px]">
+          <CartTotal/>
+          <div className="w-full text-end">
+            <button onClick={()=>navigate('/place-order')} className="bg-black  text:sm text-white my-8 px-8 py-3 cursor-pointer">PROCEED TO CHECKOUT</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
